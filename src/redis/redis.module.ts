473b8c5f54189eb3ad533cbfab1968bc64d23c22
Basanta <basantaparajuli.11@gmail.com>
2025-04-26 10:55:44 +0545

    setting up redis-client in module!

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import Redis from 'ioredis';
 
-@Module({})
+const RedisProvider = {
+  provide: 'REDIS_CLIENT',
+  useFactory: () => {
+    return new Redis();
+  },
+};
+
+@Module({
+  providers: [RedisProvider],
+  exports: [RedisProvider],
+})
 export class RedisModule {}
