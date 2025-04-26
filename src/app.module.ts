569753dcabcf924aaccfbf768a309841ab3138c0
Basanta <basantaparajuli.11@gmail.com>
2025-04-26 10:43:25 +0545

    the development starts now!

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { CommonModule } from './common/common.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, CommonModule, RedisModule],
 })
 export class AppModule {}
