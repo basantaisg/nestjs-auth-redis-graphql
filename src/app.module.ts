8d010bc997e9e5340e8edcdce9ecb6c2d5f4221c
Basanta <basantaparajuli.11@gmail.com>
2025-04-26 10:46:22 +0545

    generating prisma and also setting up graphqlmodule in our main app module!

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,21 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { CommonModule } from './common/common.module';
 import { RedisModule } from './redis/redis.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 
 @Module({
-  imports: [AuthModule, UserModule, CommonModule, RedisModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: './graphql/schema.gql',
+      driver: ApolloDriver,
+      sortSchema: true,
+      context: ({ req }) => ({ req }), // for auth guards !
+    }),
+    AuthModule,
+    UserModule,
+    CommonModule,
+    RedisModule,
+  ],
 })
 export class AppModule {}
