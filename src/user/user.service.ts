5ad237d46e17ff8d9b7dec5d548f8d8018ee0043
Basanta <basantaparajuli.11@gmail.com>
2025-04-26 11:15:04 +0545

    setting up  users-service!

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserInput } from './dtos/create-user.input';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createUser(data: CreateUserInput) {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    return this.prisma.user.create({
+      data: {
+        email: data.email,
+        username: data.username,
+        name: data.name,
+        password: hashedPassword,
+      },
+    });
+  }
+
+  async findUserByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+
+  async findUserById(id: string) {
+    return this.prisma.user.findUnique({
+      where: { id },
+    });
+  }
+}
